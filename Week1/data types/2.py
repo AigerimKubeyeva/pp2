7e89b0a71a2c6827cca45a92c51baed97683674d
= <kubeeva2014@mail.ru>
2023-01-26 16:19:23 +0600

    First commit

---
+++ Week1/data types/2.py
@@ -0,0 +1,43 @@
+x = 1j 
+print(x) 
+print(type(x))
+
+y = ["apple", "banana", "cherry"] 
+print(y) 
+print(type(y))
+
+z = range(6)
+print(z)
+print(type(z))
+
+a = {"name" : "John", "age" : 36}	
+print(a) 
+print(type(a))
+
+b = {"apple", "banana", "cherry"}	
+print(b)
+print(type(b))
+
+c = frozenset({"apple", "banana", "cherry"})	
+print (c) 
+print(type(c))
+
+d = True	
+print (d) 
+print(type(d))
+
+e = b"Hello"
+print (e) 
+print(type(e))	
+
+f = bytearray(5)
+print (f) 
+print(type(f))
+
+r = memoryview(bytes(5))
+print (r) 
+print(type(r))
+
+n = None 
+print (n) 
+print(type(n))
