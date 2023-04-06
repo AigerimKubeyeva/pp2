59248ac7dc3906147e6eead2a0559f03827ba712
= <kubeeva2014@mail.ru>
2023-04-06 13:07:40 +0600

    lab8

--- Week8/3.py
+++ Week8/3.py
@@ -6,43 +6,43 @@ import pygame
 clock = pygame.time.Clock()
 fps = 60  
 
-RED = (255, 0, 0)  # colors
+RED = (255, 0, 0) 
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
-SPEED = 6  # speed of enemy car
-SCORE = 0  # player's score
+SPEED = 6 
+SCORE = 0  
 
-WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 393, 600  # size of the window
-pygame.init()  # initialization
-screen = pygame.display.set_mode(WINDOW_SIZE)  # initializing main screen
+WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 393, 600 
+pygame.init()
+screen = pygame.display.set_mode(WINDOW_SIZE)  
 
-font = pygame.font.SysFont('Verdana', 63)  # big font
-font_small = pygame.font.SysFont('Verdana', 18)  # small font
-game_over_text_label = font.render('Game over!', True, WHITE)  # game over label
-background = pygame.image.load('street.png')  # background image
+font = pygame.font.SysFont('Verdana', 63) 
+font_small = pygame.font.SysFont('Verdana', 18)  
+game_over_text_label = font.render('Game over!', True, WHITE) 
+background = pygame.image.load('street.png')
 pygame.mixer.init()  
 
 pygame.display.set_caption('GAME')  
 
-game_over = False  # game not over condition
+game_over = False  
 
 
 class Coin(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('coin.png')  # image of coin
-        self.rect = self.image.get_rect()  # rectangle of image
+        self.image = pygame.image.load('coin.png')  
+        self.rect = self.image.get_rect()
         self.set_random_position()  
 
     def move(self):
-        self.rect.move_ip(0, 3)  # it is moving by y-axis with step 3
+        self.rect.move_ip(0, 3)
         if self.rect.top > WINDOW_HEIGHT: 
             self.set_random_position()
 
     def draw(self, surface):
-        surface.blit(self.image, self.rect)  # displaying coin
+        surface.blit(self.image, self.rect)  
 
     def set_random_position(self):
         self.rect.center = (random.randint(65, WINDOW_WIDTH - 65), 0)
@@ -51,16 +51,16 @@ class Coin(pygame.sprite.Sprite):
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('enemy.png')  # image of enemy car
-        self.rect = self.image.get_rect()  # rectangle of image
+        self.image = pygame.image.load('enemy.png')  
+        self.rect = self.image.get_rect() 
         self.set_random_position()
 
     def move(self):
         global SPEED, SCORE
-        self.rect.move_ip(0, SPEED)  # car is moving by y-axis with some speed
+        self.rect.move_ip(0, SPEED) 
 
-        if self.rect.top > WINDOW_HEIGHT:  # if enemy car reached the bottom of window
-            SCORE += 1  # player get point for not collision with enemy car
+        if self.rect.top > WINDOW_HEIGHT: 
+            SCORE += 1 
             self.set_random_position()
 
     def set_random_position(self):
@@ -68,107 +68,100 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.bottom = 0  
 
     def draw(self, surface):
-        surface.blit(self.image, self.rect)  # displaying the enemy car
+        surface.blit(self.image, self.rect)  
 
 
-class Player(pygame.sprite.Sprite):  # Player is moving by x-axis. It can collide with coin or enemy car
+class Player(pygame.sprite.Sprite):  
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('player.png')  # image of player
-        self.rect = self.image.get_rect()  # rectangle of image
-        self.rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT - 50)  # initial coordinates of center
-
+        self.image = pygame.image.load('player.png')  
+        self.rect = self.image.get_rect()  
+        self.rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT - 50)
     def move(self):
-        pressed_keys = pygame.key.get_pressed()  # player moves by x-axis when some keys are pressed
-
-        if self.rect.left > 43:  # possible coordinate by x-axis is limited by yellow line
-            if pressed_keys[pygame.K_LEFT]:  # if left arrow touched, car will move to the left direction
+        pressed_keys = pygame.key.get_pressed()  
+        if self.rect.left > 43:  
+            if pressed_keys[pygame.K_LEFT]: 
                 self.rect.move_ip(-5, 0)
         if self.rect.right < WINDOW_WIDTH - 43:
-            if pressed_keys[pygame.K_RIGHT]:  # if right arrow touched, car will move to the right direction
+            if pressed_keys[pygame.K_RIGHT]:  
                 self.rect.move_ip(5, 0)
 
     def draw(self, surface):
-        surface.blit(self.image, self.rect)  # displaying the player
+        surface.blit(self.image, self.rect) 
 
 
-player = Player()  # initializing the object of player class
-enemy = Enemy()  # init the object of enemy class
-coin = Coin()  # init the object of coin class
+player = Player()  
+enemy = Enemy()  
+coin = Coin()  
 
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 all_sprites.add(enemy)
 all_sprites.add(coin)
 
-enemies = pygame.sprite.Group()  # pygame sprite collide method requires array of elements with which player can collide
+enemies = pygame.sprite.Group()
 enemies.add(enemy)
 
-coins = pygame.sprite.Group()  # also, we need special array of coin sprites
+coins = pygame.sprite.Group()  
 coins.add(coin)
 
-INC_SPEED = pygame.USEREVENT + 1  # increasing speed of enemy car event
-pygame.time.set_timer(INC_SPEED, 4000)  # the period of increasing speed is 4s
+INC_SPEED = pygame.USEREVENT + 1  
+pygame.time.set_timer(INC_SPEED, 4000)  
+captured_coins = 0  
 
-captured_coins = 0  # variable which keeps number of touched coins
 
-
-def generated_new_coin():  # generate coin
+def generated_new_coin():  
     global coin
-    coin = Coin()  # we create new coin after the player takes previous
+    coin = Coin()
     coins.add(coin)
     all_sprites.add(coin)
 
 
 
-while not game_over:  # while game is playing
-    for event in pygame.event.get():  # events
-        if event.type == pygame.QUIT:  # quit
+while not game_over:  
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
             game_over = True
-        if event.type == INC_SPEED:  # increase speed by 0.5
+        if event.type == INC_SPEED: 
             SPEED += 0.5
 
-    screen.blit(background, (0, 0))  # blitting background image to the window
+    screen.blit(background, (0, 0)) 
 
     screen.blit(
         font_small.render(f'Score: {SCORE}', True, BLUE), (293, 10)
-    )  # displaying total score
-
+    )  
     screen.blit(
         font_small.render(f'Coins: {captured_coins}', True, BLUE), (293, 30)
-    )  # displaying number of touched coins
-
+    )  
     for sprite in all_sprites:
-        sprite.move()  # moving and displaying all sprites
+        sprite.move()  
         sprite.draw(screen)
 
-    if pygame.sprite.spritecollideany(player, coins):  # if player get coin
-        coin.kill()  # coin disappear
-        captured_coins += 1  # variable of number of touched coins
-        SCORE += 1  # for touching a coin player get one point
-        generated_new_coin()  # new coin will appear
+    if pygame.sprite.spritecollideany(player, coins): 
+        coin.kill()
+        captured_coins += 1 
+        SCORE += 1  
+        generated_new_coin() 
 
-    if pygame.sprite.spritecollideany(player, enemies):  # collision with enemy car
+    if pygame.sprite.spritecollideany(player, enemies):  
         pygame.mixer.music.stop()
-        pygame.mixer.Sound('crash.wav').play()  # crash music appears
-        time.sleep(1)  # wait of 1 second
+        pygame.mixer.Sound('crash.wav').play() 
+        time.sleep(1) 
 
-        screen.fill((69, 172, 116))  # unique green color on a backgroung
-        screen.blit(game_over_text_label, (12, 190))  # displaying message that game over
-        screen.blit(font_small.render(f'Your score: {SCORE}', True, WHITE), (15, 270))  # display score
+        screen.fill((69, 172, 116))
+        screen.blit(game_over_text_label, (12, 190))  
+        screen.blit(font_small.render(f'Your score: {SCORE}', True, WHITE), (15, 270))  
         screen.blit(font_small.render(f'Captured coins: {captured_coins}', True, WHITE),
-                    (15, 290))  # display number of touched coins
+                    (15, 290)) 
 
-        pygame.display.update()  # updating the screen for showing game over page
-
+        pygame.display.update() 
         for sprite in all_sprites:
-            sprite.kill()  # stopping the game
+            sprite.kill()  
+        time.sleep(7)  
 
-        time.sleep(7)  # wait for 7 seconds
+        game_over = True 
 
-        game_over = True  # condition of game
-
-    pygame.display.update()  # changing the position of objects
-    clock.tick(fps)  # speed of changing the screen
+    pygame.display.update()  
+    clock.tick(fps)  
 
 pygame.quit()
